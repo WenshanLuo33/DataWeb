7c2d331420df61b3ea1bb49816f0009367678925
WenshanLuo33 <wl763@cornell.edu>
2025-03-11 15:23:22 -0400

    Update map.js（大小写错误！

--- Assignment_03Adjustment/Map03/map.js
+++ Assignment_03Adjustment/Map03/map.js
@@ -18,7 +18,7 @@ map.on("load", function () {
 
     map.addSource("women-parliament-data", {
         type: "geojson",
-        data: "../Map01/data/Women_in_Parliament.geojson"
+        data: "../Map01/data/women_in_parliament.geojson"
     });
 
     // ✅ 线性渐变颜色映射（从 Colab Altair 代码转换）
