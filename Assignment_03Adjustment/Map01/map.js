dd7db8884a323ca6eed4518df56f8b31e399a2ff
WenshanLuo33 <wl763@cornell.edu>
2025-03-11 15:22:47 -0400

    Update map.js（大小写错误！

--- Assignment_03Adjustment/Map01/map.js
+++ Assignment_03Adjustment/Map01/map.js
@@ -18,7 +18,7 @@ map.on("load", function () {
 
     map.addSource("women-parliament-data", {
         type: "geojson",
-        data: "../Map01/data/Women_in_Parliament.geojson"
+        data: "../Map01/data/women_in_parliament.geojson"
     });
 
     // ✅ Jenks Natural Breaks（你的 Colab 计算出的分级）
